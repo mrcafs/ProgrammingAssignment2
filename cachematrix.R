a34fa0fbcb9ceae98f0693e7a62cfe7f1a605d54
mrcafs <50243991+mrcafs@users.noreply.github.com>
2019-06-03 21:59:51 +0100

    first commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions create a Matrix object, from a square matrix passed to argument x,
+## and cache it into memory, so it can re-used without having to be calculated again, to get
+## the inverse of that matrix.
 
-## Write a short comment describing this function
+
+##	This function creates a square "matrix" object and caches it into memory.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+		m <- NULL
 
+		set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+      
+		get <- function() x
+        
+		setinverse <- function(solve) m <<- solve
+        
+		getinverse <- function() m
+        
+		list(	set = set,
+				get = get,
+				setinverse = setinverse,
+				getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##	This function calculates the inverse of an object created using makeCacheMatrix.
+##	If the inverse has already been calculated, then it is retrieved from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+		m <- x$getinverse()
+        
+		if(!is.null(m)) {
+                
+				message("getting cached data")
+                
+				return(m)
+        }
+        
+		data <- x$get()
+        
+		m <- solve(data, ...)
+        
+		x$setinverse(m)
+        
+		m
 }
+
+
+
